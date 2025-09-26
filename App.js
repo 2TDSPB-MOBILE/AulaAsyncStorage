73f3878418a1a7d885c10c4b5a91a4c35b2353b4
Prof. Fernando Pinéo <pf1963@fiap.com.br>
2025-09-26 11:58:12 -0300

    Finalizado a função editarProduto, incluindo a lib de Mask para o campo preço do produto, e finalizado com a configuração da tela de splash-screen

--- App.js
+++ App.js
@@ -1,44 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput,TouchableOpacity,FlatList, Alert } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, FlatList, Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { TextInputMask } from 'react-native-masked-text';
 
 
 
 export default function App() {
-  const[nomeProduto,setNomeProduto]=useState("")
-  const[precoProduto,setPrecoProduto]=useState()
-  const[listaProdutos,setListaProdutos]=useState([])
-  const[produtoEditado,setProdutoEditado]=useState(null)
+  const [nomeProduto, setNomeProduto] = useState("")
+  const [precoProduto, setPrecoProduto] = useState()
+  const [listaProdutos, setListaProdutos] = useState([])
+  const [produtoEditado, setProdutoEditado] = useState(null)
 
   //Chama a função buscarDados no inicio do app
-  useEffect(()=>{
+  useEffect(() => {
     buscarDados()
-  },[])
+  }, [])
 
-  function editarProduto(index){
+  function editarProduto(index) {
     const produto = listaProdutos[index]
     setNomeProduto(produto.nome)
     setPrecoProduto(produto.preco)
-    setProdutoEditado({index}) //Define o índice do produto editado
+    setProdutoEditado({ index }) //Define o índice do produto editado
   }
 
-  async function salvar(){
+  async function salvar() {
     let produtos = [] //Inicializa um array vazio...
 
     //Verificar se o AsyncStorage já possui dados
-    if(await AsyncStorage.getItem("PRODUTOS")!==null){
+    if (await AsyncStorage.getItem("PRODUTOS") !== null) {
       //Se houver dados, será enviado para o array produtos
       produtos = JSON.parse(await AsyncStorage.getItem("PRODUTOS"))
     }
 
-    //Adicionar o produto no array
-    produtos.push({nome:nomeProduto,preco:precoProduto})
+    if (produtoEditado) {
+      //Atualizar o produto através do índice
+      produtos[produtoEditado.index] = { nome: nomeProduto, preco: precoProduto }
+    } else {
+      //Adicionar o produto no array
+      produtos.push({ nome: nomeProduto, preco: precoProduto })
+    }
+
+
 
     //Salvando os dados no Async Storage
-    await AsyncStorage.setItem("PRODUTOS",JSON.stringify(produtos))
+    await AsyncStorage.setItem("PRODUTOS", JSON.stringify(produtos))
 
-    Alert.alert("Sucesso","Produto Cadastrado com Sucesso!")
+    alert(produtoEditado ? "PRODUTO ATUALIZADO" : "PRODUTO CADASTRADO")
+    setProdutoEditado(null)
+    //Alert.alert("Sucesso", "Produto Cadastrado com Sucesso!")
 
     //Limpando os TextInputs depois de salvar
     setNomeProduto("")
@@ -56,50 +66,53 @@ export default function App() {
 
   //Função para deletar um produto
   async function deletarProduto(index) {
-      const tempDados = listaProdutos //Criando uma cópia da lista atual 
-      Alert.alert("CONFIRMAR EXCLUSÃO","Realmente deseja excluir?",[
-        {
-          text:"Cancelar",
-          style:"cancel"
-        },
-        {
-          text:"Sim",
-          style:"default",
-          onPress:async()=>{
-            const dados = tempDados.filter((item,ind)=>{
-            return ind!==index;
-            })
-            setListaProdutos(dados)
-            await AsyncStorage.setItem("PRODUTOS",JSON.stringify(dados))
-            }
+    const tempDados = listaProdutos //Criando uma cópia da lista atual 
+    Alert.alert("CONFIRMAR EXCLUSÃO", "Realmente deseja excluir?", [
+      {
+        text: "Cancelar",
+        style: "cancel"
+      },
+      {
+        text: "Sim",
+        style: "default",
+        onPress: async () => {
+          const dados = tempDados.filter((item, ind) => {
+            return ind !== index;
+          })
+          setListaProdutos(dados)
+          await AsyncStorage.setItem("PRODUTOS", JSON.stringify(dados))
         }
-      ])
-      
+      }
+    ])
+
   }
 
   return (
     <View style={styles.container}>
       <Text>CADASTRO DE PRODUTOS</Text>
       {/* TextInput do Nome do Produto */}
-      <TextInput 
+      <TextInput
         placeholder='Digite o nome produto'
         style={styles.input}
         value={nomeProduto}
-        onChangeText={(value)=>setNomeProduto(value)}
+        onChangeText={(value) => setNomeProduto(value)}
       />
 
       {/* TextInput do Preço do Produto */}
-      <TextInput 
+      <TextInputMask
+        type={'money'}
         placeholder='Digite o preço do produto'
         style={styles.input}
         keyboardType='numeric'
         value={precoProduto}
-        onChangeText={(value)=>setPrecoProduto(value)}
+        onChangeText={(value) => setPrecoProduto(value)}
       />
 
       {/* Botão para cadastrar os produtos */}
       <TouchableOpacity style={styles.btn} onPress={salvar}>
-        <Text style={styles.buttonText}>CADASTRAR</Text>
+        <Text style={styles.buttonText}>
+          {produtoEditado ? "ATUALIZAR" : "CADASTRAR"}
+        </Text>
       </TouchableOpacity>
 
       {/* Botão para buscar os dados */}
@@ -107,56 +120,56 @@ export default function App() {
         <Text style={styles.buttonText}>BUSCAR DADOS</Text>
       </TouchableOpacity>
 
-      <FlatList 
+      <FlatList
         data={listaProdutos}
-        renderItem={({item,index})=>{
-          return(
+        renderItem={({ item, index }) => {
+          return (
             <View
               style={{
-                borderWidth:1,
-                width:300,
-                borderRadius:15,
-                marginVertical:3,
-                alignItems:"center",
-                justifyContent:"center"
+                borderWidth: 1,
+                width: 300,
+                borderRadius: 15,
+                marginVertical: 3,
+                alignItems: "center",
+                justifyContent: "center",
+                padding: 5
               }}
-            >           
+            >
               <Text>PRODUTO:{item.nome} - PREÇO:{item.preco}</Text>
-              <View style={{flexDirection:'row'}}>
+              <View style={{ flexDirection: 'row' }}>
                 <TouchableOpacity
                   style={{
-                    backgroundColor:"red",
-                    borderRadius:15,
-                    width:100,
-                    alignItems:"center",
-                    justifyContent:'center',
-                    height:25
+                    backgroundColor: "red",
+                    borderRadius: 15,
+                    width: 100,
+                    alignItems: "center",
+                    justifyContent: 'center',
+                    height: 25
                   }}
-                  onPress={()=>deletarProduto(index)}
+                  onPress={() => deletarProduto(index)}
                 >
-                  <Text style={{color:'white'}}>Excluir</Text>
+                  <Text style={{ color: 'white' }}>Excluir</Text>
                 </TouchableOpacity>
-
                 <TouchableOpacity
                   style={{
-                    backgroundColor:"orange",
-                    borderRadius:15,
-                    width:100,
-                    alignItems:"center",
-                    justifyContent:'center',
-                    height:25,
-                    marginLeft:10
+                    backgroundColor: "orange",
+                    borderRadius: 15,
+                    width: 100,
+                    alignItems: "center",
+                    justifyContent: 'center',
+                    height: 25,
+                    marginLeft: 10
                   }}
-                  onPress={()=>editarProduto(index)}
+                  onPress={() => editarProduto(index)}
                 >
-                  <Text style={{color:'white'}}>Editar</Text>
+                  <Text style={{ color: 'white' }}>Editar</Text>
                 </TouchableOpacity>
               </View>
             </View>
           )
         }}
       />
-     
+
     </View>
   );
 }
@@ -167,26 +180,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'flex-start',
-    marginTop:30
+    marginTop: 30
   },
-  input:{
-    borderWidth:1,
-    height:50,
-    width:300,
-    borderRadius:15,
-    marginBottom:10
+  input: {
+    borderWidth: 1,
+    height: 50,
+    width: 300,
+    borderRadius: 15,
+    marginBottom: 10
   },
-  btn:{
-    borderWidth:1,
-    height:50,
-    width:300,
-    borderRadius:15,
-    backgroundColor:'blue',
-    justifyContent:'center',
-    alignItems:'center',
-    marginBottom:10
+  btn: {
+    borderWidth: 1,
+    height: 50,
+    width: 300,
+    borderRadius: 15,
+    backgroundColor: 'blue',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10
   },
-  buttonText:{
-    color:'white'
+  buttonText: {
+    color: 'white'
   }
 });
